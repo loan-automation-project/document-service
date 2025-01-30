d708c14386b68d7fe44b003fe16831a56216579a
Joel-jojo15 <joeljohnson404@gmail.com>
2025-01-30 12:17:51 +0530

    mapping commit

--- src/main/java/com/project/document/controller/DocumentController.java
+++ src/main/java/com/project/document/controller/DocumentController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.project.document.service.S3Service;
 
 @RestController
+@RequestMapping("/documents")
 public class DocumentController {
 
     @Autowired
